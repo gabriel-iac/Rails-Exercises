2e502e31c1b5df8980596b5aed91530581b4ebfb
gabriel-iac <gabriele.iacopetti@gmail.com>
2016-02-27 14:26:37 +0000

    created structure

--- final_project_rails/config/routes.rb
+++ final_project_rails/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/delete'
+
   devise_for :users
   root to: "home#index"
 end
--- final_project_rails/db/schema.rb
+++ final_project_rails/db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212165706) do
+ActiveRecord::Schema.define(version: 20151212170359) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
     t.string  "title"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20151212165706) do
     t.integer "user_id"
   end
 
-  add_index "movies", ["user_id"], name: "index_movies_on_user_id"
+  add_index "movies", ["user_id"], name: "index_movies_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,7 +43,8 @@ ActiveRecord::Schema.define(version: 20151212165706) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "movies", "users"
 end
